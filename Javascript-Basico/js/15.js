3e136987083fc8c2f1a49d58491d837e64b969f1
Samuel Jones <131406046+Samueljones1712@users.noreply.github.com>
2024-11-05 16:52:57 -0600

    add ECMAScript Modules

---
+++ Javascript-Basico/js/15.js
@@ -0,0 +1,6 @@
+//ECMAScript Modules
+import { sumar, restar } from "./functions.js" //Siempre la extension del archivo
+
+const resultado = sumar(20,10)
+
+console.log(resultado)
---
+++ Javascript-Basico/js/functions.js
@@ -0,0 +1,13 @@
+/*
+export const sumar = (n1, n2) => {
+    return n1+n2
+} */
+
+export function sumar(n1, n2){
+    return n1+n2
+}
+
+
+export function restar(n1, n2){
+    return n1-n2
+}
